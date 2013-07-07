4b2c79e3cbb522bb74f1d7f1b5926c3df1d44016
Charly-70 <clopezsar@yahoo.es>
2013-07-07 16:12:53 +0000

    Added buffer to string to web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require ('fs');
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send( fs.readFile('index.html') );
+  response.send( buf.toString( fs.readFile('index.html') ) );
 });
 
 var port = process.env.PORT || 5000;
